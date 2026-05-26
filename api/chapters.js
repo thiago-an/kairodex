524732fb06e3d9f7faa66fe45df5b8a121746a8b
Thiago Araujo <thiagoangelino11@gmail.com>
2026-05-26 20:30:00 -0300

    mantem capitulos somente ptbr

--- api/chapters.js
+++ api/chapters.js
@@ -8,9 +8,15 @@ export default async function handler(req, res) {
   try {
     const { id } = req.query;
 
+    if (!id) {
+      return res.status(400).json({ error: "ID do mangá não enviado." });
+    }
+
     const params = new URLSearchParams();
+
     params.append("manga", id);
     params.append("limit", "100");
+    params.append("translatedLanguage[]", "pt-br");
     params.append("order[chapter]", "desc");
 
     const response = await fetch(
@@ -20,6 +26,7 @@ export default async function handler(req, res) {
     const data = await response.json();
 
     res.status(200).json(data);
+
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
